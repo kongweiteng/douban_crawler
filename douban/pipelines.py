eea48fc77d711d0e5229e49161dbc445a2907750
kongweiteng <kongweiteng@enn.cn>
2019-04-01 23:12:57 +0800

    修改定义mongo的连接信息

--- douban/pipelines.py
+++ douban/pipelines.py
@@ -6,16 +6,18 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import pymongo
-from douban.settings import mongo_host, mongo_port, mongo_db_name, mongo_db_collection,mongo_user,mongo_password
+from douban.settings import mongo_host, mongo_port, mongo_db_name, mongo_db_collection, mongo_user, mongo_password
 
 
 class DoubanPipeline(object):
     def __init__(self):
         host = mongo_host
         port = mongo_port
+        user = mongo_user
+        password = mongo_password
         dbname = mongo_db_name
         sheetname = mongo_db_collection
-        client = pymongo.MongoClient('mongodb://{0}:{1}@{2}:{3}'.format(mongo_user,mongo_password,mongo_host,mongo_port))
+        client = pymongo.MongoClient('mongodb://{0}:{1}@{2}:{3}'.format(user, password, host, port))
         mydb = client[dbname]
         self.post = mydb[sheetname]
 
