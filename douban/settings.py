66e8556b3a991572e7194ab0d7e6de774984e226
kongweiteng <kwt19921992>
2019-04-09 16:24:28 +0800

    es save

--- douban/settings.py
+++ douban/settings.py
@@ -23,15 +23,15 @@ ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # 开启线程数量，默认16
-CONCURRENT_REQUESTS = 100
+CONCURRENT_REQUESTS = 20
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 0
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 100
-CONCURRENT_REQUESTS_PER_IP = 100
+CONCURRENT_REQUESTS_PER_DOMAIN = 20
+CONCURRENT_REQUESTS_PER_IP = 20
 LOG_LEVEL = 'INFO'
 startDate = datetime.datetime.now().strftime('%Y%m%d')
 #LOG_FILE = f"./log/mySpiderlog{startDate}.txt"
